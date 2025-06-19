0a1b30353e2aa814fd2d28bfa1bce5f1cc8f6d1a
radhika <97277770+BADGE28@users.noreply.github.com>
2025-06-19 09:00:28 +0530

    student login is complete

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -17,5 +17,80 @@ const generateAccessAndRefreshTokens = async(studentId) => {
 }
 
 const registerStudent = asyncHandler(async (req, res) => {
+    const {sname,rollNumber, username, password,email} = req.body;
+
+    if(
+        [sname, password, rollNumber, email, username].some((field) => field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedStudent = await Student.findOne({$or: [{rollNumber}, {username}, {email}]})
+
+    if(existedStudent) {
+        throw new ApiError(409, "Student with this roll number or username already exists");
+    }
     
- })
+    const student = new Student({ rollNumber, username, email, sname, password });
+    let result = await student.save();
+
+    // Generate JWT tokens
+    const tokens = await generateAccessAndRefreshTokens(result._id);
+
+    // Remove password from response
+    result = result.toObject();
+    delete result.password;
+
+    // Final response
+    res.status(201).json(
+        new ApiResponse(201, { student: result, tokens }, "Student registered successfully")
+    );
+});
+
+const studentLogin = asyncHandler(async (req, res) => {
+    const {rollNumber, password, username, email} = req.body;
+    if(!rollNumber && !username && !email){
+        throw new ApiError(400, "Roll number, username or email is required");
+    }
+
+    const student = await Student.findOne({
+        $or: [{rollNumber}, {username}, {email}]
+    });
+    
+    if(!student){
+        throw new ApiError(404, "Student not found");
+    }
+
+    const isPasswordValid = await student.isPasswordCorrect(password);
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid student credentials");
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(student._id);
+
+    const loggedInStudent = await Student.findById(student._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200, 
+            {
+                student: loggedInStudent,accessToken,
+                refreshToken
+            },
+            "Student logged in successfully"
+        )
+    )
+})
+
+const getStudentDetail = asyncHandler(async (req, res) => {
+    
+})
+
