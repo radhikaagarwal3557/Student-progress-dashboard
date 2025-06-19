b259732146e77b5ed6bae828345295047633faf9
radhika <97277770+BADGE28@users.noreply.github.com>
2025-06-19 13:04:20 +0530

    update student is done

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -111,6 +111,102 @@ const logoutStudent = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Student logged out successfully"));
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken ||  req.body.refreshToken;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "unauthorized request");
+    }
+
+    try{
+        const decodedtoken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET
+        )
+
+        const student = await Student.findById(decodedtoken?.id);
+
+        if(!student){
+            throw new ApiError(401, "invalid refresh token");
+        }
+
+        if(incomingRefreshToken !== student?.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or used");
+        }
+
+        const options = {
+            httpOnly: true,
+            secure: true,
+        }
+
+        const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(student._id);
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200, 
+                {accessToken, refreshToken:newRefreshToken},
+                "Access token refreshed"
+            )
+        )    
+    } catch (error) {
+        throw new ApiError(401,error?.message ||  "Invalid refresh token");
+    }
+})
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {oldPasswod, newPassword} = req.body;
+
+    const student = await Student.findById(req.student._id);
+    const isPasswordCorrect = await student.isPasswordCorrect(oldPasswod);
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    student.password = newPassword;
+    await student.save({validateForSave: false});
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+})
+
+const updateStudent = asyncHandler(async (req, res) => {
+    const {sname, email, password} = req.body;
+
+    if(!sname || !email){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const updatedData = {sname, email};
+
+    if (password) {
+        updatedData.password = await bcrypt.hash(password, 10);
+    }
+
+    const updatedStudent = await Student.findByIdAndUpdate(
+        req.student?._id,
+        {
+            $set: {
+                ...updatedData,
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    if (!updatedStudent) {
+        throw new ApiError(404, "Student not found");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedstudent, "Student updated successfully"));
+})
+
 const getStudentDetail = asyncHandler(async (req, res) => {
     const { id } = req.params;
 
