1b60668a951e3273530fa3efd8b4b9899ae43904
radhika <97277770+BADGE28@users.noreply.github.com>
2025-06-21 18:46:50 +0530

    student route is complete

---
+++ backend/src/routes/student.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import {
+    registerStudent,
+    studentLogin,
+    logoutStudent,
+    refreshAccessToken,
+    changeCurrentPassword,
+    updateStudent,
+    getStudentDetail,
+    deleteStudent
+} from '../controllers/studentController.js';
+
+import {verifyJWT} from '../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.post('/register', registerStudent);
+router.route('/login').post(studentLogin);
+router.route('/logout').post(verifyJWT, logoutStudent);
+router.route('/refresh-token').post(refreshAccessToken);
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/update').put(verifyJWT, updateStudent);
+router.route('/detail/:id').get(verifyJWT, getStudentDetail);
+router.route('/delete').delete(verifyJWT, deleteStudent);
