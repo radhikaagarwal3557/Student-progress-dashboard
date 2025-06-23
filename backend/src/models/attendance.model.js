3f3448f7a931c7e1f72c8c11c8f68b25adf04a20
radhika <97277770+BADGE28@users.noreply.github.com>
2025-06-23 12:40:28 +0530

    attendance model is complete

--- backend/src/models/attendance.model.js
+++ backend/src/models/attendance.model.js
@@ -1,2 +1,37 @@
 import mongoose, {Schema} from 'mongoose';
 
+const attendanceSchema = new Schema({
+    student:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'student',
+        required: true
+    },
+    teacher:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'teacher',
+        required: true
+    },
+    subName:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subject',
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Present', 'Absent'],
+        required: true
+    },
+    subName: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'subject',
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Attendance = mongoose.model('attendance', attendanceSchema);
