b4a3310828d1f6e82619e049a6255d09eaa2007b
radhika <97277770+BADGE28@users.noreply.github.com>
2025-07-02 12:40:20 +0530

    attendance list is complete

--- frontend/studentprogressdashboard/src/App.jsx
+++ frontend/studentprogressdashboard/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPages'; 
 import StudentList from './pages/StudentList';
+import AttendanceList from './pages/AttendanceList';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/students" element={<StudentList />} />
+        <Route path="/student" element={<StudentList />} />
+        <Route path="/attendance" element={<AttendanceList />} />
       </Routes>
     </BrowserRouter>
   );
